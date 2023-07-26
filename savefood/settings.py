90d63737bf76292ab1b77d632274d7add2adfe89
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-07-26 09:32:03 +0000

    refactor: Refactor settings for local hosting

--- savefood/settings.py
+++ savefood/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['save-food-3b2a71b51608.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['save-food-3b2a71b51608.herokuapp.com', '8000-samuelanderson-savefood-van3cv57nm2.ws-eu102.gitpod.io']
 
 
 # Application definition
