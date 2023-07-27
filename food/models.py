79669788552ee3436a85e390bc05e8d35fc580d5
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-07-27 13:10:55 +0000

    feat: Add model for posts

--- food/models.py
+++ food/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    """
+    class for posts
+    inheriting from the python model
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="food_item_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    item_description = models.TextField(blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+
+class Meta:
+    """
+    This class orders posts
+    on the created on field
+    """
+    ordering = ['-created_on']
+
+
+def __str__(self):
+    """
+    returns custom string object representation for posts
+    instead of the default
+    """
+    return self.title
+
