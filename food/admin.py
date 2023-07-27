0bd991e3d830e107850ffa31053b86203679bf22
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-07-27 16:32:32 +0000

    feat: Customize the posts list and slug admin views

--- food/admin.py
+++ food/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    This class customizes the list view of posts 
+    and pre-populates slugs from the title field
+    It also helps to filter post searches
+    """
+    list_display = ('title', 'slug', 'status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    search_fields = ('title', 'content')
