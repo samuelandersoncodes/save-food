e30cdf4c37ab0f3377a13806b0e9cd56611ea640
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-07-28 10:24:03 +0000

    feat: Add class for post view

--- food/views.py
+++ food/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    """
+    This class inherits from generic view
+    filter posts by status
+    order them by creation time and date
+    and paginates the post page
+    """
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 8
