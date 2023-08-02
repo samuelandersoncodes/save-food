2fd4e1b71d537d585cc4476ed0988efc252deac5
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-02 08:40:03 +0000

    feat: Add postdetail class to views

--- food/views.py
+++ food/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -14,3 +14,26 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 8
+
+
+class PostDetail(View):
+    """
+    This class gets the food item posts by slug
+    filters out the active ones
+    post them and their associated comments on the
+    postdetail page
+    """
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+            },
+        )
