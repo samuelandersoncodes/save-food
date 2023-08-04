8cfba8421bb532632e05f750e82e3d3550f691e5
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-04 17:48:17 +0000

    build: Install and wire up allauth

--- savefood/settings.py
+++ savefood/settings.py
@@ -41,10 +41,20 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django.contrib.staticfiles',
     'cloudinary',
     'food',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- savefood/urls.py
+++ savefood/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('food.urls'), name='food_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
