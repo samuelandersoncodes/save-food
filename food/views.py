3adb1e8efd204cb859921cd13caed41ce90e7732
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-10 20:40:44 +0000

    build: Import and render comment form for get method

--- food/views.py
+++ food/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -17,14 +18,14 @@ class PostList(generic.ListView):
 
 
 class PostDetail(View):
-    """
-    This class gets the food item posts by slug
-    filters out the active ones
-    post them and their associated comments on the
-    postdetail page
-    """
 
     def get(self, request, slug, *args, **kwargs):
+        """
+        This function gets the food item posts by slug
+        filters out the active ones
+        post them and their associated comments on the
+        postdetail page
+        """
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
@@ -35,5 +36,7 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
+                "commented": False,
+                "comment_form": CommentForm()
             },
         )
