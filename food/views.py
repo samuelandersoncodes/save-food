5c490f7dbca0436f44867f3187d71b6f422c7edf
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-10 20:48:53 +0000

    feat: Add post function to render comment

--- food/views.py
+++ food/views.py
@@ -40,3 +40,36 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+        """
+        this function gets data from the user form
+        filters approved comments
+        checks for user form validity
+        asigns comment to its respective post
+        and save the comment
+        or return an empty field if invalid
+        """
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
+            },
+        )
