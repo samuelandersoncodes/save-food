45300763c4651f832b1e2fba3a9300bfed8b85a2
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-18 18:13:21 +0000

    perf: Set up whitenoise to load statics

--- savefood/settings.py
+++ savefood/settings.py
@@ -98,6 +98,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'savefood.urls'
