900c31257f4710af2478e8188297114c86948e96
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-18 14:23:35 +0000

    fix: Alter urls to fix name error

--- food/urls.py
+++ food/urls.py
@@ -1,7 +1,11 @@
 from . import views
-from django.urls import path, include
+from django.urls import path
+from .views import PostDetail, AddPost
+
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('addpost/', AddPost.as_view(), name='add_post'),
+    path('post_detail/<slug:slug>/',
+         views.PostDetail.as_view(), name='post_detail'),
 ]
