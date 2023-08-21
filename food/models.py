b3c667adfee75b725da6e92a0f68e1dc0fcda970
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-21 16:13:10 +0000

    refactor: Change default comment approval to true

--- food/models.py
+++ food/models.py
@@ -63,7 +63,7 @@ class Comment(models.Model):
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
+    approved = models.BooleanField(default=True)
 
 
 class Meta:
@@ -80,4 +80,3 @@ def __str__(self):
     instead of the default
     """
     return f"Comment {self.body} by {self.name}"
-
