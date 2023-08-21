ae6ffdf7d99f38ef5dd008b1f006762ec10274e4
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-21 15:18:19 +0000

    refactor: Alter home page pagination

--- food/views.py
+++ food/views.py
@@ -18,7 +18,7 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
-    paginate_by = 8
+    paginate_by = 9
 
 
 class PostDetail(View):
