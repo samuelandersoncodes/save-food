d9bf208daca05f7d5c648a5062263e59f070e850
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-22 10:31:40 +0000

    fix: Modify path to fix 404 error

--- food/urls.py
+++ food/urls.py
@@ -6,7 +6,7 @@ from .views import PostDetail, AddPost, EditPost
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('addpost/', AddPost.as_view(), name='add_post'),
-    path('editpost/', EditPost.as_view(), name='edit_post'),
+    path('editpost/<slug:pk>/', EditPost.as_view(), name='edit_post'),
     path('post_detail/<slug:slug>/',
          views.PostDetail.as_view(), name='post_detail'),
 ]
