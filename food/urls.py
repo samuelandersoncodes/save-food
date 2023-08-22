b41f3b6cde09ee96a3252675e34fe1c7dc632084
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-22 09:10:04 +0000

    feat: Add editpost path

--- food/urls.py
+++ food/urls.py
@@ -6,6 +6,7 @@ from .views import PostDetail, AddPost
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('addpost/', AddPost.as_view(), name='add_post'),
+    path('editpost/', EditPost.as_view(), name='edit_post'),
     path('post_detail/<slug:slug>/',
          views.PostDetail.as_view(), name='post_detail'),
 ]
