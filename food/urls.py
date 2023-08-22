61a966d17619bef7e87970ebf76ae79c31f5988b
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-22 12:42:12 +0000

    fix: Import deletepost to fix name error

--- food/urls.py
+++ food/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-from .views import PostDetail, AddPost, EditPost
+from .views import PostDetail, AddPost, EditPost, DeletePost
 
 
 urlpatterns = [
