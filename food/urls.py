b30d9acbb35212fba455c2b1e3dfa6aca8ab67d8
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-22 12:32:34 +0000

    feat: Add path for deletepost

--- food/urls.py
+++ food/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('addpost/', AddPost.as_view(), name='add_post'),
     path('editpost/<slug:pk>/', EditPost.as_view(), name='edit_post'),
+    path('deletepost/<slug:pk>/delete',
+         DeletePost.as_view(), name='delete_post'),
     path('post_detail/<slug:slug>/',
          views.PostDetail.as_view(), name='post_detail'),
 ]
