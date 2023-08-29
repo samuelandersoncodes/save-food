b567d3a8555032f38cb8e939fdc636844451b6b7
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-29 19:28:19 +0000

    test: Add test for post detail view

--- food/test_views.py
+++ food/test_views.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from .models import Post
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class TestViews(TestCase):
@@ -12,3 +15,22 @@ class TestViews(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+
+    def test_post_detail(self):
+        """
+        this function creates a user and post instance
+        gets the post detail page url
+        and comfrims that its response status is good
+        """
+        newuser = User.objects.create(username='test', password='test')
+        post = Post.objects.create(
+            title='Banana',
+            slug='banana',
+            author=newuser,
+            item_description='sweet banana',
+            status=1,
+            address='Berlin, 122345')
+        response = self.client.get('/post_detail/{post.slug}/')
+        url = reverse('post_detail', kwargs={'slug': post.slug})
+        self.response = self.client.get(url)
+        self.assertEqual(self.response.status_code, 200)
