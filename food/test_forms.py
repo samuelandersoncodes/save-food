48bf2aa6c6b96d760783cd7767c77d0a6e7730c8
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-29 12:09:30 +0000

    test: Add test for post form

--- food/test_forms.py
+++ food/test_forms.py
@@ -26,3 +26,27 @@ class TestCommentForm(TestCase):
         form = CommentForm()
         self.assertEqual(form.Meta.fields, ('body',))
 
+
+class TestPostForm(TestCase):
+    """ post form test """
+    def test_title_field_is_required(self):
+        """
+        this function instatiates a form without a title
+        checks its validity which turns out to be not valid
+        and finds the title key in the dictionary of form errors
+        then checks for the associated error message
+        """
+        form = PostForm({'title': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        """
+        this function ensures that
+        other fields either than the specified ones
+        are not automatically loaded
+        """
+        form = PostForm()
+        self.assertEqual(form.Meta.fields, [
+            'title', 'featured_image', 'item_description', 'address'])
