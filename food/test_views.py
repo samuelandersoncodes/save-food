55835ebc5fc1bbf812b2813dd428f6c85b3cdef3
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-08-30 07:49:35 +0000

    test: Add test for deletepost

--- food/test_views.py
+++ food/test_views.py
@@ -63,3 +63,22 @@ class TestViews(TestCase):
         url = reverse('edit_post', kwargs={'pk': post.pk})
         self.response = self.client.get(url)
         self.assertEqual(self.response.status_code, 200)
+
+    def test_delete_post(self):
+        """
+        this function creates a user and post instance
+        gets the deletepost page url
+        and comfrims that its response status is good
+        """
+        newuser = User.objects.create(username='test', password='test')
+        post = Post.objects.create(
+            title='Banana',
+            slug='banana',
+            author=newuser,
+            item_description='sweet banana',
+            status=1,
+            address='Berlin, 122345')
+        response = self.client.get('/deletepost/{post.pk}/')
+        url = reverse('delete_post', kwargs={'pk': post.pk})
+        self.response = self.client.get(url)
+        self.assertEqual(self.response.status_code, 200)
