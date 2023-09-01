5a9271c3567cab5fbc60d237380850e28bc34f37
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-09-01 10:50:16 +0000

    feat: Add method for nav item activeness

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,17 @@ setTimeout(function () {
     let alertEl = new bootstrap.Alert(messages);
     alertEl.close();
 }, 3000);
+
+// Listens to nav activities and asigns its activeness respectively 
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    // This method activates nav elements upon click
+    const navItems = document.getElementsByClassName("nav-item");
+    const removeActiveAll = () => {
+        for (let item of navItems) {
+            if (item.classList.contains("active"))
+                item.classList.remove("active");
+        }
+    };
+
+});
