4cdb8854a02e3ec7ce4916c99b2798d46e8d7be2
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2023-09-08 18:12:36 +0000

    refactor: Refactor code for jshint validation

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,11 @@
+// jshint esversion: 6
+
 // Injects real world date into copyright date of footer
 document.getElementById('copyright-date').innerHTML = (new Date().getFullYear());
 
 // set timeout for message alert displays
 setTimeout(function () {
-    let messages = document.getElementById('message')
+    let messages = document.getElementById('message');
     let alertEl = new bootstrap.Alert(messages);
     alertEl.close();
 }, 3000);
